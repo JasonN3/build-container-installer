a3a57757786f129f59ff39c82a76597d9daf21d0
Jason N. <33561705+JasonN3@users.noreply.github.com>
2024-02-13 16:00:37 +0000

    find based on original path

--- scripts/gen_xorriso.sh
+++ scripts/gen_xorriso.sh
@@ -6,10 +6,10 @@ echo "-boot_image any replay"
 echo "-volid Fedora-S-dvd-x86_64-39"
 echo "-joliet on"
 echo "-compliance joliet_long_names"
-cache_files=$(find registry_cache)
+cache_files=$(find /registry_cache)
 for file in ${cache_files}
 do
-    echo "-map $(pwd)/${file} ${file}"
+    echo "-map $(pwd)${file} ${file}"
     echo "-chmod 0444 ${file}"
 done
 echo "-end"
