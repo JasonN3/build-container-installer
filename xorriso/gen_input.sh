1f1cd5efd9cb7629dd2dd7577c8dcd551fc79bbc
Jason N. <33561705+JasonN3@users.noreply.github.com>
2024-02-14 14:00:36 +0000

    save outside runtime

---
+++ xorriso/gen_input.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "-indev $(pwd)/results/images/boot.iso"
+echo "-outdev $(pwd)/results/images/deploy.iso"
+echo "-boot_image any replay"
+echo "-volid Fedora-S-dvd-x86_64-39"
+echo "-joliet on"
+echo "-compliance joliet_long_names"
+echo "-map $(pwd)/base-main-39.tar base-main-39.tar"
+echo "-chmod 0444 base-main-39.tar"
+echo "-end"
