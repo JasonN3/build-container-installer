fa53488780874b1ab8395f8cf779590c2d1bd5d9
Jason N. <33561705+JasonN3@users.noreply.github.com>
2024-03-04 18:43:06 -0500

    copy ars before shifting

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,8 @@ set -ex
 # Create /dev/loop0 if it doesn't already exist. `losetup` has an issue creating it during the first run
 mknod -m 0660 /dev/loop0 b 7 0 2>/dev/null || true
 
+args=$@
+
 while (( "$#" ))
 do
   export "$1"
@@ -15,10 +17,10 @@ done
 make container/${IMAGE_NAME}-${IMAGE_TAG} $@
 
 # Build base ISO
-make boot.iso $@
+make boot.iso $args
 
 # Add container to ISO
-make build/deploy.iso $@
+make build/deploy.iso $args
 
 # Make output dir in github workspace
 mkdir /github/workspace/build || true
