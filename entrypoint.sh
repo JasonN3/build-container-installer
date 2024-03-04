5df6762b678490eb60e5ed72a5c41691c5ef3bfc
Jason N. <33561705+JasonN3@users.noreply.github.com>
2024-03-04 18:24:08 -0500

    support spaces

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,10 @@ set -ex
 # Create /dev/loop0 if it doesn't already exist. `losetup` has an issue creating it during the first run
 mknod -m 0660 /dev/loop0 b 7 0 2>/dev/null || true
 
-for entry in $@
+while (( "$#" ))
 do
-  export $entry
+  export "$1"
+  shift
 done
 
 # Pull container
