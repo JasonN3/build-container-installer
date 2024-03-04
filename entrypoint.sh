4dbbc7dd2f0a3fea9851bd4b077c66a65af8824b
Jason N. <33561705+JasonN3@users.noreply.github.com>
2024-03-04 18:50:20 -0500

    use parameter expansion

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,22 +5,19 @@ set -ex
 # Create /dev/loop0 if it doesn't already exist. `losetup` has an issue creating it during the first run
 mknod -m 0660 /dev/loop0 b 7 0 2>/dev/null || true
 
-args=$@
-
-while (( "$#" ))
+for i
 do
-  export "$1"
-  shift
+  export ${i@Q}
 done
 
 # Pull container
 make container/${IMAGE_NAME}-${IMAGE_TAG} $@
 
 # Build base ISO
-make boot.iso $args
+make boot.iso $@
 
 # Add container to ISO
-make build/deploy.iso $args
+make build/deploy.iso $@
 
 # Make output dir in github workspace
 mkdir /github/workspace/build || true
