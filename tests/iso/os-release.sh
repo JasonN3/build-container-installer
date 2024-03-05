c4e378d8aed2ef83cd3595b41040d5401bdb7c09
Jason N. <33561705+JasonN3@users.noreply.github.com>
2024-03-04 20:16:07 -0500

    add sudo

--- tests/iso/os-release.sh
+++ tests/iso/os-release.sh
@@ -2,21 +2,22 @@
 
 iso=$1
 
-dnf install -y squashfs-tools
+sudo apt-get update
+sudo apt-get install -y squashfs-tools
 
-mkdir /mnt/{iso,install}
+sudo mkdir /mnt/{iso,install}
 
 # Mount ISO
-mount -o loop $iso /mnt/iso
+sudo mount -o loop $iso /mnt/iso
 
 # Mount squashfs
-mount -t squashfs -o loop /mnt/iso/images/install.img /mnt/install
+sudo mount -t squashfs -o loop /mnt/iso/images/install.img /mnt/install
 
 FOUND_VERSION=$(cat /mnt/install/os-release | grep VERSION_ID | cut -d= -f2)
 
 # Cleanup
-umount /mnt/install
-umount /mnt/iso
+sudo umount /mnt/install
+sudo umount /mnt/iso
 
 if [[ ${FOUND_VERSION} != ${VERSION} ]]
 then
