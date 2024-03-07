f462ab7c049240bc1379b9db16e77acd99fa2de2
Jason N. <33561705+JasonN3@users.noreply.github.com>
2024-03-07 17:27:57 -0500

    findme

--- tests/iso/verify_flatpakrepo_updated.sh
+++ tests/iso/verify_flatpakrepo_updated.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-export
-
-set -ex
-
 add_line=$(grep flatpak_manager.add_remote /mnt/install/usr/lib64/python*/site-packages/pyanaconda/modules/payloads/payload/rpm_ostree/flatpak_installation.py)
 
 add_line_repo=$(echo ${add_line} | grep ${FLATPAK_REMOTE_NAME})
@@ -12,14 +8,18 @@ add_line_url=$(echo ${add_line} | grep ${_FLATPAK_REPO_URL})
 result=0
 if [ -z "${add_line_repo}" ]
 then
-    echo "Repo name not updated"
+    echo "Repo name not updated on add_remote line"
     result=1
+else
+    echo "Repo name found on add_remote line"
 fi
 
 if [ -z "${add_line_url}" ]
 then
-    echo "Repo url not updated"
+    echo "Repo url not updated on add_remote line"
     result=1
+else
+    echo "Repo url found on add_remote line"
 fi
 
 replace_line=$(grep flatpak_manager.replace_installed_refs_remote /mnt/install/usr/lib64/python*/site-packages/pyanaconda/modules/payloads/payload/rpm_ostree/flatpak_installation.py)
@@ -28,8 +28,10 @@ replace_line_repo=$(echo ${replace_line} | grep ${FLATPAK_REMOTE_NAME})
 
 if [ -z "${replace_line_repo}" ]
 then
-    echo "Repo name not updated in second line"
+    echo "Repo name not updated on replace_installed_refs line"
     result=1
+else
+    echo "Repo name found on replace_installed_refs line"
 fi
 
 exit ${result}
