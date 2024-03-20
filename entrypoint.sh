d5873a6af582a3744f5b0ce62adb56dc19793b9c
Jason N. <33561705+JasonN3@users.noreply.github.com>
2024-03-20 12:02:40 -0400

    ignore everything

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -ex
 # Create /dev/loop0 if it doesn't already exist. `losetup` has an issue creating it during the first run
 mknod -m 0660 /dev/loop0 b 7 0 2>/dev/null || true
 
-dnf check-update > /dev/null
+dnf check-update > /dev/null || true
 
 for i
 do
