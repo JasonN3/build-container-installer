a38f8a8d565e9bb6074a15a462d089a0a8446102
Jason N. <33561705+JasonN3@users.noreply.github.com>
2024-03-20 09:57:58 -0400

    consistent var names and refresh repos

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,8 @@ set -ex
 # Create /dev/loop0 if it doesn't already exist. `losetup` has an issue creating it during the first run
 mknod -m 0660 /dev/loop0 b 7 0 2>/dev/null || true
 
+subscription-manager refresh || true
+
 for i
 do
   key=$(echo ${i} | cut -d= -f1)
