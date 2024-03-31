76edeb887c6a5db890c9ae56cdaee33ba8be7161
Jason N. <33561705+JasonN3@users.noreply.github.com>
2024-03-31 10:56:18 -0400

    double $ and show expected

--- test/iso/install_hash.sh
+++ test/iso/install_hash.sh
@@ -4,3 +4,10 @@ set -ex
 
 checkisomd5 ../../${ISO_NAME}
 cd $(dirname ../../${ISO_NAME}) && sha256sum -c $(basename ${ISO_NAME})-CHECKSUM
+if [[ $? != 0 ]]
+then
+    echo "Found:"
+    checkisomd5 --md5sumonly ../../${ISO_NAME}
+    echo "Expected:"
+    implantisomd5 --force base-39.iso
+fi
