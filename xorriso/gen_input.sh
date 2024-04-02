9fec285df02b315c08318efaec229c314c0a0b30
Jason N. <33561705+JasonN3@users.noreply.github.com>
2024-04-02 16:36:57 -0400

    fix relative path

--- xorriso/gen_input.sh
+++ xorriso/gen_input.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "-report_about WARNING"
-echo "-indev ${PWD}/results/images/boot.iso"
+echo "-indev ${PWD}/../results/images/boot.iso"
 echo "-outdev ${ISO_NAME}"
 echo "-boot_image any replay"
 echo "-joliet on"
@@ -23,7 +23,7 @@ then
     pushd "${FLATPAK_DIR}" > /dev/null
     for file in $(find * -type f)
     do
-        echo "-map $(pwd)/${file} flatpak/${file}"
+        echo "-map ${PWD}/${file} flatpak/${file}"
         echo "-chmod 0444 flatpak/${file}"
     done
     popd > /dev/null
@@ -31,14 +31,14 @@ fi
 
 if [ -f $(pwd)/sb_pubkey.der ]
 then
-	echo "-map $(pwd)/sb_pubkey.der sb_pubkey.der"
+	echo "-map $(pwd)/../sb_pubkey.der sb_pubkey.der"
 	echo "-chmod 0444 /sb_pubkey.der"
 fi
 
-pushd "${PWD}/container" > /dev/null
+pushd "${PWD}/../container" > /dev/null
 for file in $(find "${IMAGE_NAME}-${IMAGE_TAG}" -type f)
 do
-    echo "-map $(pwd)/${file} ${file}"
+    echo "-map ${PWD}/${file} ${file}"
     echo "-chmod 0444 ${file}"
 done
 popd > /dev/null
